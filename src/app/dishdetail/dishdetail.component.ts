684f81e08985767605b0070eb10649c6a3679cb3
aashimalik <ayeshamalik8751@gmail.com>
2018-05-27 13:26:37 +0530

    WIP assignment 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,7 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,11 +17,28 @@ export class DishdetailComponent implements OnInit {
   dishIds: number[];
   prev: number;
   next: number;
-
+  feedbackForm: FormGroup;
+  formErrors = {
+    'name': '',
+    'comment': ''
+  };
+  validationMessages = {
+    'name': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.',
+      'maxlength':     'Name cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required':      'comment is required.'
+    }
+  };
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -29,6 +47,45 @@ export class DishdetailComponent implements OnInit {
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     // const id = +this.route.snapshot.params['id'];
     // this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
+  }
+
+  createForm() {
+    this.feedbackForm = this.fb.group({
+       name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+       comment: ['',  Validators.required],
+       rating:''
+     });
+     this.feedbackForm.valueChanges
+     .subscribe(data => this.onValueChanged(data));
+ 
+     this.onValueChanged(); // (re)set validation messages now
+   }
+
+   onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        // tslint:disable-next-line:forin
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.feedbackForm.value;
+    console.log(this.feedbackForm.value);
+     this.feedbackForm.reset({
+      name: '',
+      rating: '',
+      comment: ''
+    });
   }
 
   setPrevNext(dishId: number) {
