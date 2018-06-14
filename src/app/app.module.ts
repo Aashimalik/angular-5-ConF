a391168bd74f2c50043ba4597c2da0eccc4d10f6
ayesha_malik <ayesha_malik@seologistics.com>
2018-06-14 10:26:28 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { baseURL } from './shared/baseurl';
 import {ProcessHttpmsgService} from './services/process-httpmsg.service';
 import { RestangularModule, Restangular } from 'ngx-restangular';
 import { RestangularConfigFactory } from './shared/restConfig';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,6 +36,7 @@ import { RestangularConfigFactory } from './shared/restConfig';
     AboutComponent,
     ContactComponent,
     LoginComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
